333f5e5c07448f6cfa4532ed55cbc0dd519bbc31
Nia Poor <70398294+niapoor@users.noreply.github.com>
2024-11-20 20:06:08 -0500

    Init

---
+++ button_text_change.gd
@@ -0,0 +1,12 @@
+extends Button
+
+#var test = email_main_instance.test;
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+	
+func _add_button_text(text):
+	self.text = text
